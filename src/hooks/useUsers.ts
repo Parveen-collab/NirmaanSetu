c2645259b7d23248d9d4d7cbb95df93be25d532c
Parveen Kumar <pk16061999@gmail.com>
2026-07-03 18:38:35 +0530

    fixed import issue

--- src/hooks/useUsers.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getUsers } from "@/src/services/userService";
-
-export const useUsers = () => {
-  return useQuery({
-    queryKey: ["users"],
-    queryFn: getUsers,
-  });
-};
