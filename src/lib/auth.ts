f2c503aad7f44b44d6873fc8ff5fac619bca44e2
Parveen Kumar <pk16061999@gmail.com>
2026-08-22 20:04:29 +0530

    added authtype

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,7 @@
 import { AuthResponse } from "@/src/types/auth";
 
+export type AuthType = 'mobile' | 'email'
+
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function refreshAccessToken(): Promise<AuthResponse | null> {
